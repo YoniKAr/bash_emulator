2bac86a98b76fb2b2d9602ab03440714ff5a8f2f
Ubuntu <hirdaesingh@hotmail.com>
2023-03-08 00:08:04 -0700

    Added CMake

---
+++ .build/CMakeFiles/emulator.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/Personal/bash_emulator/bash_file_sys.cpp" "CMakeFiles/emulator.dir/bash_file_sys.cpp.o" "gcc" "CMakeFiles/emulator.dir/bash_file_sys.cpp.o.d"
+  "/home/ubuntu/Personal/bash_emulator/emulator.cpp" "CMakeFiles/emulator.dir/emulator.cpp.o" "gcc" "CMakeFiles/emulator.dir/emulator.cpp.o.d"
+  "/home/ubuntu/Personal/bash_emulator/terminal.cpp" "CMakeFiles/emulator.dir/terminal.cpp.o" "gcc" "CMakeFiles/emulator.dir/terminal.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ .build/CMakeFiles/emulator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emulator.dir/bash_file_sys.cpp.o"
+  "CMakeFiles/emulator.dir/bash_file_sys.cpp.o.d"
+  "CMakeFiles/emulator.dir/emulator.cpp.o"
+  "CMakeFiles/emulator.dir/emulator.cpp.o.d"
+  "CMakeFiles/emulator.dir/terminal.cpp.o"
+  "CMakeFiles/emulator.dir/terminal.cpp.o.d"
+  "emulator"
+  "emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
